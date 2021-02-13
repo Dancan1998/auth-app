bc706cdab6d596499123659d3a34c6d848b01fe4
Dancan Chibole <dancankingstar@gmail.com>
2021-02-13 11:51:28 -0800

    login a user

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin)
 from .managers import CustomUserManager
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -24,4 +26,8 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
     def tokens(self):
-        return ''
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, status, views
 from rest_framework.response import Response
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.conf import settings
@@ -23,3 +23,13 @@ class RegisterAPIView(generics.GenericAPIView):
         user = User.objects.get(email=user_data['email'])
 
         return Response(user_data, status=status.HTTP_201_CREATED)
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
