19d74b135e6733552ba84a74931991bc38237487
Dancan Chibole <dancankingstar@gmail.com>
2021-02-13 10:43:06 -0800

    custom user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin)
+from .managers import CustomUserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = None
+    email = models.EmailField(unique=True, db_index=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    county = models.CharField(max_length=150)
+    id_no = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        return ''
